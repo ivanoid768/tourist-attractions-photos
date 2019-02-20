83c81fd087b5e7a83a1993c105e1deee828e3014
ivanoid256 <ivanoid256@india.com>
2019-02-20 13:45:53 +0500

    .gitignore /src and /test

---
+++ build/precache-manifest.0b2cd0f5962dc71bddbcb0b8d028f9e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "599a9788a71e808d92a8",
+    "url": "/tourist-attractions-photos/static/js/runtime~main.599a9788.js"
+  },
+  {
+    "revision": "221f87f0697211267682",
+    "url": "/tourist-attractions-photos/static/js/main.221f87f0.chunk.js"
+  },
+  {
+    "revision": "0c9eefb6b004de59600c",
+    "url": "/tourist-attractions-photos/static/js/1.0c9eefb6.chunk.js"
+  },
+  {
+    "revision": "221f87f0697211267682",
+    "url": "/tourist-attractions-photos/static/css/main.86767a99.chunk.css"
+  },
+  {
+    "revision": "0c9eefb6b004de59600c",
+    "url": "/tourist-attractions-photos/static/css/1.8b8c6c08.chunk.css"
+  },
+  {
+    "revision": "51b2708dfbf51ec694bec559e26a2f18",
+    "url": "/tourist-attractions-photos/index.html"
+  }
+];
