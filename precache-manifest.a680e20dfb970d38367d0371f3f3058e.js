5066cffd367dbdf19b5432e7866ee7fcec2f401f
ivanoid256 <ivanoid256@india.com>
2019-02-22 14:10:32 +0500

    Redirect to heroku

--- precache-manifest.a680e20dfb970d38367d0371f3f3058e.js
+++ precache-manifest.a680e20dfb970d38367d0371f3f3058e.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/tourist-attractions-photos/static/js/runtime~main.599a9788.js"
   },
   {
-    "revision": "8bfd15c6b2c7eedad657",
-    "url": "/tourist-attractions-photos/static/js/main.8bfd15c6.chunk.js"
+    "revision": "cf8f9abd3be6b68aa843",
+    "url": "/tourist-attractions-photos/static/js/main.cf8f9abd.chunk.js"
   },
   {
     "revision": "0c9eefb6b004de59600c",
     "url": "/tourist-attractions-photos/static/js/1.0c9eefb6.chunk.js"
   },
   {
-    "revision": "8bfd15c6b2c7eedad657",
+    "revision": "cf8f9abd3be6b68aa843",
     "url": "/tourist-attractions-photos/static/css/main.86767a99.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/tourist-attractions-photos/static/css/1.8b8c6c08.chunk.css"
   },
   {
-    "revision": "7f03dddf847a2c04d805f52b65da94e0",
+    "revision": "a167601407fb38b241d7d05e71d4a465",
     "url": "/tourist-attractions-photos/index.html"
   }
 ];
